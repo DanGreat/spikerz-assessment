9e2f7971480ed502f1b678eda9055d1c5e532491
Daniel <dbassey360@gmail.com>
2025-10-21 15:10:21 +0100

    Added functionality to the paginator of the contextual risk table

--- src/app/features/dashboard/components/dashboard-analytics/contextual-risk-table/contextual-risk-table.ts
+++ src/app/features/dashboard/components/dashboard-analytics/contextual-risk-table/contextual-risk-table.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Paginator, PaginatorState } from 'primeng/paginator';
 import { TableModule } from 'primeng/table';
 import { IAsset } from '../../../../../core/models/asset';
@@ -12,13 +12,23 @@ import { StatusBadge } from '../../../../../shared/components/status-badge/statu
   styleUrl: './contextual-risk-table.scss',
 })
 export class ContextualRiskTable {
-  assests = signal<IAsset[]>([
+  allAssets = signal<IAsset[]>([
     { name: 'Loremipsumdolorsit', ip: '192.168.1.1', status: 'Critical' },
     { name: 'Loremipsumdolorsit002', ip: '192.168.1.2', status: 'Critical' },
+    { name: 'Loremipsumdolorsit', ip: '192.168.1.1', status: 'High' },
+    { name: 'Loremipsumdolorsit002', ip: '192.168.1.2', status: 'High' },
+    { name: 'Loremipsumdolorsit', ip: '192.168.1.1', status: 'Medium' },
+    { name: 'Loremipsumdolorsit002', ip: '192.168.1.2', status: 'Medium' },
   ]);
 
   first = signal<number>(0);
   rows = signal<number>(2);
+
+  pagedAssets = computed(() => {
+    const start = this.first();
+    const end = start + this.rows();
+    return this.allAssets().slice(start, end);
+  });
 
   onPageChange(event: PaginatorState) {
     this.first.set(event.first ?? 0);
